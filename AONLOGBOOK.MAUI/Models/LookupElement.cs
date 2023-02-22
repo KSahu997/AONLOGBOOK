80e6a1ae96ac13943cd721512f7318862dbfd42d
karan.sahu <karan.sahu@aondigicon.com>
2023-02-22 13:29:18 +0530

    asd

---
+++ AONLOGBOOK.MAUI/Models/LookupElement.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AONLOGBOOK.MAUI.Models
+{
+    public class LookupElement
+    {
+        public string paramName { get; set; }
+        public string paramValue { get; set; }
+    }
+}
