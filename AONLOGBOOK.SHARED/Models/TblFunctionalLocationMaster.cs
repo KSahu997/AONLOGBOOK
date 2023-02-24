13f3f0378926460f8255bd7c9e341261960d2866
smritimoni29 <smritimoni29@gmail.com>
2023-02-24 13:19:17 +0530

    Dynamic form and report updates

---
+++ AONLOGBOOK.SHARED/Models/TblFunctionalLocationMaster.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AONLOGBOOK.SHARED.Models
+{
+    public partial class TblFunctionalLocationMaster
+    {
+        public Guid Id { get; set; }
+        public string? FunctionalLocation { get; set; }
+        public string? LocationCode { get; set; }
+        public string? WorkCenterId { get; set; }
+        public string? CompanyId { get; set; }
+        public string? PlantId { get; set; }
+        public int? DelFlag { get; set; }
+        public string? UpdatedBy { get; set; }
+        public DateTime? UpdatedOn { get; set; }
+        public string? InsetedBy { get; set; }
+        public DateTime? InsertedOn { get; set; }
+    }
+}
