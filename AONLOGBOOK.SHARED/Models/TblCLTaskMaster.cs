645b564c73d21eff36f2dbc2bd585132eaa5ba33
imabhik97s <abhaykaushik333@gmail.com>
2023-02-28 15:09:16 +0530

    taskmastertable

---
+++ AONLOGBOOK.SHARED/Models/TblCLTaskMaster.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AONLOGBOOK.SHARED.Models
+{
+	public class TblCLTaskMaster
+	{
+		public Guid ID { get; set; }
+
+		public string? CompanyID { get; set; }
+
+		public string? Based_on { get; set; }
+
+		public string? Action { get; set; }
+
+		public int? Del_Flag { get; set; }
+
+		public string? Updated_By { get; set; }
+
+		public DateTime? Updated_On { get; set; }
+
+		public string? Inserted_By { get; set; }
+
+		public DateTime? Inserted_On { get; set; }
+	}
+}
