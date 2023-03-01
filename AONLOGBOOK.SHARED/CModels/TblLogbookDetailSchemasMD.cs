2a87a2c2809d1409c265b4f635189828f927710c
smritimoni29 <smritimoni29@gmail.com>
2023-03-01 12:46:11 +0530

    logdetailSchema updates

---
+++ AONLOGBOOK.SHARED/CModels/TblLogbookDetailSchemasMD.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AONLOGBOOK.SHARED.CModels
+{
+    public class TblLogbookDetailSchemasMD
+    {
+        public Guid Id { get; set; }
+        public string? Tag_Name { get; set; }
+        public string? Display_Name { get; set; }
+        public string? Source { get; set; }
+        public double L_Min { get; set; }
+        public double L_Max { get; set; }
+        public string? DataType { get; set; }
+       // public string? Id { get; set; }
+        public int Seq { get; set; }
+        public string? Element { get; set; }
+        public int isMandatory { get; set; }
+        public string? LogbookId { get; set; }
+       
+        // public string? Unit { get; set; }
+        public string? UOM { get; set; }
+        public int? Show { get; set; }
+        public int? Prscn { get; set; }
+
+        public string? RefCol { get; set; }
+
+        public string? Operator { get; set; }
+
+        public string? Critical { get; set; }
+
+        public string? Special { get; set; }
+
+        public int? isDashboardComponent { get; set; }
+        public string? CalulationParams { get; set; }
+
+      
+        public DateTime? CretedOn { get; set; }
+
+        public string? CreatedBy { get; set; }
+
+        public DateTime? UpdatedOn { get; set; }
+
+        public string? UpdatedBy { get; set; }
+
+        public int? Del_Flag { get; set; }
+
+
+    }
+}
--- AONLOGBOOK.SHARED/Models/TblLogbookDetailSchema.cs
+++ AONLOGBOOK.SHARED/Models/TblLogbookDetailSchema.cs
@@ -7,17 +7,17 @@ namespace AONLOGBOOK.SHARED.Models
     {
         public Guid Id { get; set; }
 
-        public string LogbookId { get; set; }
+        public string? LogbookId { get; set; }
 
-        public string Element { get; set; }
+        public string ?Element { get; set; }
 
-        public string DataType { get; set; }
+        public string ?DataType { get; set; }
 
-        public string UOM { get; set; }
+        public string? UOM { get; set; }
 
         public int? Show { get; set; }
 
-        public string Source { get; set; }
+        public string ?Source { get; set; }
 
         public float? L_Max { get; set; }
 
@@ -27,25 +27,25 @@ namespace AONLOGBOOK.SHARED.Models
 
         public DateTime? CretedOn { get; set; }
 
-        public string CreatedBy { get; set; }
+        public string ?CreatedBy { get; set; }
 
         public DateTime? UpdatedOn { get; set; }
 
-        public string UpdatedBy { get; set; }
+        public string? UpdatedBy { get; set; }
 
         public int? Del_Flag { get; set; }
 
-        public string RefCol { get; set; }
+        public string? RefCol { get; set; }
 
-        public int Seq { get; set; }
+        public int? Seq { get; set; }
 
-        public string Calulation_Params { get; set; }
+        public string ?Calulation_Params { get; set; }
 
-        public string Operator { get; set; }
+        public string? Operator { get; set; }
 
-        public string Critical { get; set; }
+        public string? Critical { get; set; }
 
-        public string Special { get; set; }
+        public string? Special { get; set; }
 
         public int? isDashboardComponent { get; set; }
 
